57682f6d93b9715715b50687c3c9a48126aa0e01
Jasperbauerbrown <jcbb500@gmail.com>
2023-01-02 20:40:50 -0700

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driveyboi.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+@TeleOp(name="Driveyboi", group="Linear Opmode")
+
+public class Driveyboi extends LinearOpMode {
+
+    BNO055IMU.Parameters imuParameters;
+    Orientation angles;
+    public BNO055IMU imu;
+    static DcMotorEx Lift;
+    static DcMotorEx FR;
+    static DcMotorEx FL;
+    static DcMotorEx BR;
+    static DcMotorEx BL;
+    int speed;
+    int LiftTarget;
+    double strafe;
+    double mult;
+
+    public void runOpMode() {
+        motorMode driveMode = new motorMode();
+        motors motors = new motors();
+        hardWareDecl();
+        driveMode.changeDriveTrain(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveMode.changeDriveTrain(DcMotor.RunMode.RUN_USING_ENCODER);
+        driveMode.setDirectionDriveTrain(DcMotorSimple.Direction.FORWARD);
+        Lift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        Lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        Lift.setDirection(DcMotorEx.Direction.REVERSE);
+        imuParameters = new BNO055IMU.Parameters();
+        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imuParameters.loggingEnabled = false;
+        imu.initialize(imuParameters);
+        telemetry.update();
+        waitForStart();
+        while (opModeIsActive()) {
+            Lift.setPower(1);
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            motors.setMotors(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x,gamepad1.right_stick_y, gamepad1.left_trigger, gamepad1.right_trigger, gamepad1.left_stick_button, gamepad1.right_stick_button);
+            lift();
+            tel();
+        }
+    }
+    public void lift() {
+        if (gamepad1.a) speed = -190;
+        else if (gamepad1.y) speed = 190;
+        else speed = 0;
+        if (speed > 0 && Lift.getCurrentPosition() >= 3210) {
+            speed = 0;
+        }
+        else if (speed < 0 && Lift.getCurrentPosition() <= 0) {
+            speed = 0;
+        }
+        if (speed != 0) LiftTarget = Lift.getCurrentPosition() + speed;
+        Lift.setTargetPosition(LiftTarget);
+
+    }
+    public void tel() {
+        telemetry.addData("Angle: ", angles.firstAngle);
+        telemetry.addData("Lift: ", Lift.getCurrentPosition());
+        telemetry.update();
+    }
+    public void hardWareDecl() {
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        Lift = hardwareMap.get(DcMotorEx.class, "Lift");
+        FR = hardwareMap.get(DcMotorEx.class, "FR");
+        FL = hardwareMap.get(DcMotorEx.class, "FL");
+        BR = hardwareMap.get(DcMotorEx.class, "BR");
+        BL = hardwareMap.get(DcMotorEx.class, "BL");
+    }
+}
